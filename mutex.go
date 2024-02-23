28e1e3df52781910d626f63561f5d8aafb1c0b37
Jason <jqqjj@qq.com>
2024-02-23 20:20:47 +0800

    申请完锁后释放pubSub

--- mutex.go
+++ mutex.go
@@ -47,6 +47,7 @@ func (m *Mutex) Lock() error {
 	defer cancel()
 
 	pubSub := m.redisClient.Subscribe(ctx, ChannelName(m.lockName))
+	defer pubSub.Close()
 	defer pubSub.Unsubscribe(ctx, ChannelName(m.lockName))
 
 	threadID := m.InstanceID + ":" + strconv.FormatInt(GoroutineID(), 10)
@@ -155,8 +156,7 @@ func init() {
 	-- KEYS[1] 锁名
 	-- ARGV[1] 协程唯一标识：客户端标识+协程ID
 	-- ARGV[2] 过期时间
-	if redis.call('exists',KEYS[1]) == 0 then
-		redis.call('set',KEYS[1],ARGV[1])
+	if redis.call('setnx',KEYS[1],ARGV[1]) == 1 then
 		redis.call('pexpire',KEYS[1],ARGV[2])
 		return nil
 	end
